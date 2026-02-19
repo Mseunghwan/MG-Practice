f2a286db98825759ecea97d302d222ed52def94f
Mseunghwan <minccc42710@gmail.com>
2026-02-20 01:41:10 +0900

    initial commit

---
+++ src/main/java/jpabook/mgpractice/MgPracticeApplication.java
@@ -0,0 +1,13 @@
+package jpabook.mgpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpabook/mgpractice/MgPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.mgpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
